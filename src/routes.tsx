a08ec87b45935ddd0156ce7b5bedf2919f3e86d7
Jadson <jadsombruno2016@gmail.com>
2021-05-25 22:19:34 -0300

    Create app routes

---
+++ src/routes.tsx
@@ -0,0 +1,12 @@
+import { Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+
+const Routes = (): JSX.Element => (
+  <Switch>
+    <Route path="/" exact component={Home} />
+    <Route path="/cart" component={Cart} />
+  </Switch>
+);
+
+export default Routes;
